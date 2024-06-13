2f43e36b3f821a447db4396914b0ef2776de79b2
godssun <98688265+godssun@users.noreply.github.com>
2024-06-13 13:58:13 +0900

    svelte, vite로 바꾼 후 firebase deploy SETTING

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Router from "svelte-spa-router";
+  import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import Sign from "./pages/Sign.svelte";
+  import Write from "./pages/Write.svelte";
+  import Notfound from "./pages/Notfound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/signup": Sign,
+    "/write": Write,
+    "*": Notfound,
+  };
+</script>
+
+<Router {routes}></Router>
---
+++ src/pages/Main.svelte
@@ -0,0 +1,77 @@
+<script>
+  let hour = 20;
+  let min = 10;
+  let sec = 0;
+
+  setInterval(function () {
+    sec = sec + 1;
+  }, 1000);
+</script>
+
+<header>
+  <div class="info-bar">
+    <div class="info-bar__time">{hour}:{min}:{sec}</div>
+    <div class="info-bar__icons">
+      <img src="assets/chart-bar.svg" alt="chart-bar" />
+      <img src="assets/wifi.svg" alt="wifi" />
+      <img src="assets/battery.svg" alt="battery" />
+    </div>
+  </div>
+  <div class="menu-bar">
+    <div class="menu-bar__location">
+      <div class="menu-bar__location-wirte">수서1동</div>
+      <div class="menu-bar__location-icon">
+        <img src="assets/arrow.svg" alt="arrow" />
+      </div>
+    </div>
+    <div class="menu-bar__icons">
+      <img src="assets/search.svg" alt="serach" />
+      <img src="assets/menu.svg" alt="menu" />
+      <div class="dot">
+        <span class="menu-bar__icons-alret">1</span>
+        <img src="assets/alert.svg" alt="alert" />
+      </div>
+    </div>
+  </div>
+</header>
+
+<!--TODO: 메인 링크부분 수정하기-->
+<main>
+  <a class="write-btn" href="#/write">+ 글쓰기</a>
+</main>
+
+<footer>
+  <div class="footer-block">
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/home.svg" alt="home" />
+      </div>
+      <div class="footer-icons__desc">홈</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/doc.svg" alt="doc" />
+      </div>
+      <div class="footer-icons__desc">동네생활</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/location.svg" alt="location" />
+      </div>
+      <div class="footer-icons__desc">내 근처</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/chat.svg" alt="chat" />
+      </div>
+      <div class="footer-icons__desc">채팅</div>
+    </div>
+    <div class="footer-icons">
+      <div class="footer-icons__img">
+        <img src="assets/user.svg" alt="user" />
+      </div>
+      <div class="footer-icons__desc">나의 당근</div>
+    </div>
+  </div>
+</footer>
+<div class="media-info-msg">Screen is too big!</div>
